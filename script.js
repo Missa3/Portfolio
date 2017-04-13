67d003ae3a25702bff6de4a4405c39803cdf0279
Missael Romero <missael7994@gmail.com>
2017-04-13 16:04:57 -0700

    Js added

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+var pics =[
+  'pics/geekwise.png',
+  'pics/blocjams.png',
+  'pics/instaclone.png',
+    'pics/PetApp.png',
+  'pics/colorgame.png',
+  'pics/todolist.png'
+];
+var projects = document.getElementsByClassName('b');
 
+for (var i = 0; i < projects.length; i++) {
+  projects[i].style.backgroundImage = "url("+pics[i]+")";
+}
